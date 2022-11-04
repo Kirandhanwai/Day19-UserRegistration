a923adf1a9ab7f71b3441e07d10e4152722b4e82
Kirandhanwai <kirandhanwai123@gmail.com>
2022-11-05 01:12:57 +0530

    Added : Check First Name Start with Captial

--- src/com/bridgelabz/RegexMain.java
+++ src/com/bridgelabz/RegexMain.java
@@ -1,5 +1,7 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegexMain {
@@ -8,7 +10,16 @@ public class RegexMain {
 
 	public static void main(String[] args) {
 
-		System.out.println("Welcome to User Registiration Program");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first name");
+		String firstName = sc.nextLine();
+
+		Matcher matcher = pattern.matcher(firstName);
+		if (matcher.matches())
+			System.out.println(firstName + " is valid");
+		else
+			System.out.println(firstName + " is not valid");
+		sc.close();
 	}
 
 }
