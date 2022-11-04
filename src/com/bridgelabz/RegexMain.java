7195818ad42a5b8e61a5ba1f9ff883c3fe8e6caa
Kirandhanwai <kirandhanwai123@gmail.com>
2022-11-05 01:52:36 +0530

    Added : Check Email is Valid or Not

--- src/com/bridgelabz/RegexMain.java
+++ src/com/bridgelabz/RegexMain.java
@@ -6,9 +6,7 @@ import java.util.regex.Pattern;
 
 public class RegexMain {
 
-	static Pattern pattern = Pattern.compile("^[A-Z][A-Za-z]{2,}");
-
-	static void check(String name) {
+	static void check(String name, Pattern pattern) {
 		Matcher matcher = pattern.matcher(name);
 		if (matcher.matches())
 			System.out.println(name + " is valid");
@@ -19,14 +17,20 @@ public class RegexMain {
 
 	public static void main(String[] args) {
 
+		Pattern namePattern = Pattern.compile("^[A-Z][A-Za-z]{2,}");
+
+		Pattern emailPattern = Pattern.compile("^[a-z]{1,}[\\.]?[a-z]*[\\@][a-z]{1,}[\\.][a-z]{1,}[\\.]?[a-z]*$");
+
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter first name");
 		String firstName = sc.nextLine();
-		check(firstName);
+		check(firstName, namePattern);
 		System.out.println("Enter Last Name");
 		String lastName = sc.nextLine();
-		check(lastName);
-
+		check(lastName, namePattern);
+		System.out.println("Enter email id Name");
+		String email = sc.nextLine();
+		check(email, emailPattern);
 		sc.close();
 	}
 
