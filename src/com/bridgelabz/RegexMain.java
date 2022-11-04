c356afe918db1f530ca582ca40e8de17c58fc7d1
Kirandhanwai <kirandhanwai123@gmail.com>
2022-11-05 01:10:14 +0530

    Added: Welcome to User Registraion Program

---
+++ src/com/bridgelabz/RegexMain.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+import java.util.regex.Pattern;
+
+public class RegexMain {
+
+	static Pattern pattern = Pattern.compile("^[A-Z][A-Za-z]{2,}");
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to User Registiration Program");
+	}
+
+}
