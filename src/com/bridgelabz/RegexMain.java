bf0f33ea6d25195646bf269cd6cddad99f718fed
Kirandhanwai <kirandhanwai123@gmail.com>
2022-11-05 01:24:05 +0530

    Added : Check Last Name Start with Captial

--- src/com/bridgelabz/RegexMain.java
+++ src/com/bridgelabz/RegexMain.java
@@ -8,17 +8,25 @@ public class RegexMain {
 
 	static Pattern pattern = Pattern.compile("^[A-Z][A-Za-z]{2,}");
 
+	static void check(String name) {
+		Matcher matcher = pattern.matcher(name);
+		if (matcher.matches())
+			System.out.println(name + " is valid");
+		else
+			System.out.println(name + " is not valid");
+
+	}
+
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter first name");
 		String firstName = sc.nextLine();
+		check(firstName);
+		System.out.println("Enter Last Name");
+		String lastName = sc.nextLine();
+		check(lastName);
 
-		Matcher matcher = pattern.matcher(firstName);
-		if (matcher.matches())
-			System.out.println(firstName + " is valid");
-		else
-			System.out.println(firstName + " is not valid");
 		sc.close();
 	}
 
